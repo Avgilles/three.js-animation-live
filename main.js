5e49da9762781c13755a89ef5fe43a55df1ef8bc
avgilles <avraamgil@gmail.com>
2025-08-19 21:56:40 +0200

    step2 cube scroled !

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+// import Math
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,12 +15,23 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+let clock = new THREE.Clock();
+
+
+let wheel_y = 0;
+
+document.addEventListener("wheel", (event) => {
+    wheel_y += event.deltaY * 0.01; // Ajuster la sensibilité
+    // console.log("Delta Y:", event.deltaY); // valeur positive ou négative
+    console.log("wheel_y:", wheel_y); // valeur positive ou négative
+  });
 
 function animate() {
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+//    cube.rotation.x += 0.01;
+//    cube.rotation.y += 0.01;
+//    cube.position.x = Math.sin(clock.getElapsedTime());
+    cube.position.z = wheel_y;
   renderer.render( scene, camera );
 
 }
