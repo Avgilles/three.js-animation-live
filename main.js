6eafedd1587c22e568256d12bb8b4c61b796efca
avgilles <avraamgil@gmail.com>
2025-09-02 21:42:51 +0200

    add emisive

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -17,46 +20,58 @@ camera.setFocalLength(10);
 const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
 
 material.emissive.set(1,1,1); 
-material.emissiveIntensity = 10;
+// material.emissiveIntensity = 10;
+material.emissiveIntensity = 0.1;
 
 
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(0, 3, 0);
-scene.add(light);
+// const light = new THREE.PointLight(0xffffff, 1);
+// light.position.set(0, 3, 0);
+// scene.add(light);
 
 camera.position.z = 5;
 
-const geometry = new THREE.BoxGeometry( 1, 5, 1 );
-let cubes = [];
-
-for(let i=0; i<4; i++){
-    const cube = new THREE.Mesh( geometry, material );
-
-    cube.rotation.set(0, 0, Math.PI*2*.25*(i+.5)); 
-
-    // cube.rotation.z += Math.PI*.5;
-    // let pos = i-2;
-    // cube.position.set(pos*2, pos*2, 0); 
+const geometry = new THREE.BoxGeometry( 2.5, 2.5, 2.5 );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 
-    scene.add( cube );
-    cubes.push(cube);
-}
-
-// cubes[0].position.x += 2;
-// cubes[0].position.y += 2;
-
-cubes[1].position.x -= 3;
-cubes[1].position.x -= 3;
 
 
-cubes[2].position.x += 3;
-cubes[2].position.y += 3;
+const BLOOM_SCENE = 1;
 
+const bloomLayer = new THREE.Layers();
+bloomLayer.set( BLOOM_SCENE );
 
+const params = {
+            threshold: 0,
+            strength: 1,
+            radius: 0.005,
+            exposure: .1
+        };
 
 const controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
+
+const renderScene = new RenderPass( scene, camera );
+
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass.threshold = params.threshold;
+bloomPass.strength = params.strength;
+bloomPass.radius = params.radius;
+
+
+const outputPass = new OutputPass();
+
+
+const bloomComposer = new EffectComposer( renderer );
+bloomComposer.renderToScreen = true;
+bloomComposer.addPass( renderScene );
+bloomComposer.addPass( bloomPass );
+bloomComposer.addPass( outputPass );
+
+
+const finalComposer = new EffectComposer( renderer );
+
+// controls.update();
 
 
 function animate() {
@@ -67,5 +82,12 @@ function animate() {
     controls.update();
 
     renderer.render( scene, camera );
+    // scene.traverse( darkenNonBloomed );
+    bloomComposer.render();
+    // scene.traverse( restoreMaterial );
+
+    // render the entire scene, then render bloom scene on top
+    finalComposer.render();
 
 }
+
